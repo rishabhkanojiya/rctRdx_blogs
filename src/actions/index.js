eeccb4b88ea6f5ff9d8f487085ea232604b67f09
Rishabh <rishabhkanojiya75@gmail.com>
2019-06-04 03:45:27 +0530

    Updaing File

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,6 @@ import _ from "lodash";
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPost());
-
   // const userIds = _.uniq(_.map(getState().posts, "userId"));
   // userIds.forEach(id => dispatch(fetchUser(id)));
   //                      ^
